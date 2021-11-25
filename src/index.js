16766ecfccd09656304ee5ed1d459902c6b5b679
Angelo Díaz <angelodiazram@gmail.com>
2021-11-25 00:44:02 -0300

    primer commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {saludar} from './js/componentes.js';
+import './style.css';
+
+const nombre = 'Angelo';
+saludar(nombre);
---
+++ webpack.prod.js
@@ -0,0 +1,77 @@
+const HtmlWebpack          = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin           = require("copy-webpack-plugin");
+const CssMinimizer         = require('css-minimizer-webpack-plugin');
+const Terser               = require('terser-webpack-plugin');
+
+module.exports = {
+
+    mode: 'production',
+
+    output: {
+        clean: true, // limpia todos los archivos y los vuelve a crear
+        filename: 'main.[contenthash].js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false,
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /style.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /style.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizer(),
+            new Terser()
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpack({
+            title: 'Clase de configuracion de Webpack',
+            filename: 'index.html',
+            template: './src/index.html'// desde que archivo tomara el esqueleto html para crear el body
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].[fullhash].css',
+            ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                {from: 'src/assets/', to: 'assets/'}          
+            ]
+        })
+    ]
+}
